98bfe5deb5e975d8d8ae8819be9d1845e10da8b8
Nguyen Tran Gia Hue <nntn1405@gmail.com>
2026-09-08 13:54:08 +0700

    feat: update tieu de v2

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ body {{ font-family: Arial; max-width: 640px; margin: 60px auto; }}
 .box {{ background:#DEEAF1; border-left: 5px solid #1F4E79; padding: 24px; border-radius: 8px; }}
 h1 {{ color: #1F4E79; }}
 </style></head><body>
-<h1>Ung dung Flask tren PaaS</h1>
+<h1>Ung dung Flask tren PaaS - phien ban 2</h1>
 <div class="box">
 <p><b>Sinh vien:</b> NGUYEN_NGOC_THAO_NGUYEN - 233404050195</p>
 <p><b>Mon hoc:</b> Dien toan Dam may </p>
